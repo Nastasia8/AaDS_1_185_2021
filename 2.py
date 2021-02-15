f5dd28a01a41466e0f58ece60f24316b07e1f9d2
anna165 <71322376+anna165@users.noreply.github.com>
2021-02-15 11:09:34 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,11 @@
+def function(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 2
+    if n==2:
+        return 5
+    return function(n-1) + function(n-2) + function(n-3)
+    
+for i in range(0,16):
+    print(function(i), end=" ")
