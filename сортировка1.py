8494a8f6a317642b8e213b1a64b66e7b67cd69ba
Nikita3221488 <71322132+Nikita3221488@users.noreply.github.com>
2021-03-01 10:32:39 +0300

    Update сортировка1.py

--- сортировка1.py
+++ сортировка1.py
@@ -1,10 +1,10 @@
 N = int(input())
-a = list(map(int, input().split(" ")))
+a = list(map(int, input().split(maxsplit=N)))
 count=0
 for i in range(N-1):
     for j in range(N-i-1):
         if a[j] > a[j+1]:
-            count+=1
+            count=1
             a[j], a[j+1] = a[j+1], a[j]
             print( " ".join( repr(e) for e in a ) )
             
