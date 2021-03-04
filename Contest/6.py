5c4391050ff51f3949658e795e3bc29ffd22317f
CruzedV <mr.dobrikv@mail.ru>
2021-03-04 18:22:50 +0300

    6 finished

--- Contest/6.py
+++ Contest/6.py
@@ -2,8 +2,10 @@
 
 n = int(input())
 list_c = input().split()
-list_c = [int(list_[i]) for i in range(n)]
+list_c = [int(list_c[i]) for i in range(n)]
 
 k = int(input())
 list_p = input().split()
-list_p = [int(list_[i]) for i in range(k)]
+list_p = [int(list_p[i]) for i in range(k)]
+
+[print("yes") if list_c[i] < list_p.count(i+1) else print("no") for i in range(0, n)]
