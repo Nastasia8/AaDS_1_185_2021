fb02bd6e65556746fcca96d7937e9fd10106a29e
DanielS005 <71259569+DanielS005@users.noreply.github.com>
2021-03-04 10:40:38 +0200

    Update contest_5.py

--- contest_5.py
+++ contest_5.py
@@ -1,3 +1,12 @@
 n = int(input('n: '))
 k = set([int(i) for i in input().split()])
 print(len(k))
+
+#2
+n = int(input('n: '))
+k = sorted([int(i) for i in input().split()])
+k2 = []
+for i in k:
+    if i not in k2:
+        k2.append(i)
+print(len(k2))
