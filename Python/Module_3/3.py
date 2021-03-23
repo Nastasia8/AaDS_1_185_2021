5dc8e43be69540e86947cf338db91719e578b662
unknown <oleg.savinov.2002@bk.ru>
2021-03-23 21:24:31 +0300

    almost success(TL on 26 test)

--- Python/Module_3/3.py
+++ Python/Module_3/3.py
@@ -1,12 +1,21 @@
+def get_hash(s):
+    p = 1e9 + 7
+    x = 26
+    res = 0
+    for i in range(len(s)):
+        res = (res*x+ord(s[i])) % p
+    return res
+
+
 s = str(input())
-index_begin = 0
 index_begin_1 = 1
-index_end = len(s)
 index_end_2 = len(s)-1
 flag = False
 k = 0
 while flag == False:
-    if s[index_begin:index_begin_1:1] == s[index_end_2:index_end:1]:
+    # if s[index_begin:index_begin_1:1] == s[index_end_2:index_end:1] and s[index_begin:index_begin_1:1]*(len(s)//(k+1)) == s:
+    #     flag = Trues
+    if get_hash(s[0:index_begin_1:1]) == get_hash(s[index_end_2:len(s):1]) and s[0:index_begin_1:1]*(len(s)//(k+1)) == s:
         flag = True
     else:
         index_begin_1 += 1
