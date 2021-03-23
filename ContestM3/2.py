24e1cf6fde3b17a9b8e819e8873c58856cd5efff
CruzedV <mr.dobrikv@mail.ru>
2021-03-23 18:08:11 +0300

    2 Done!

--- ContestM3/2.py
+++ ContestM3/2.py
@@ -1,30 +1,36 @@
-
 def hash_(string, x, p):
     result = 0
     for i in range(len(string)):
         result = (result*x + ord(string[i])) % p
     return result 
 
-
-def cyclic_shift(string_s, string_t, x, p):
-    shift_power = 0
-    string_t = " ".join(string_t)
-    string_s = " ".join(string_s)
-    hs = hash_(string_s, x, p)
-    ht = hash_(string_t, x, p)
-    for i in range(len(string_s)+1):
-        if string_t != string_s:
-            part = string_t.partition(' ')
-            string_t = part[-1] + part[1] + part[0]
-            shift_power += 1
-            ht = hash_(string_t, x, p)
-            if shift_power == len(string_s):
-                return -1
-        else:
+def cyclic_shift(s, t, x, p):
+    shift_power = 0 
+    hs = hash_(s, x, p)
+    ht = hash_(t, x, p)
+    xt = 1
+    for i in range(len(t)):
+        xt = (xt*x) % p
+    for i in range(len(s)):
+        if hs == ht:
             return shift_power
+        else:
+            shift_power += 1
+            ht = (ht*x - ord(t[i])*xt + ord(t[i]) + p) % p
+    return -1
 
-string_s = input()
-string_t = input()
-x = 27
-p = 1e9 + 7
-print(cyclic_shift(string_s, string_t, x, p))
+
+def main():
+    p = 1e9 + 7
+    x = 27
+    s = input()
+    t = input()
+    if len(t) == 1 and len(s) == 1:
+        if s == t:
+            print(0)
+        else:
+            print(-1)
+    else:
+        print(cyclic_shift(s, t, x, p))
+
+main()
