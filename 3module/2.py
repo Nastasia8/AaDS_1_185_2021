123583664790ba02ec5386dcfaca955db323f1de
zipliZ <xle64ik@gmail.com>
2021-03-24 09:31:15 +0300

    2 from contest

--- 3module/2.py
+++ 3module/2.py
@@ -8,15 +8,15 @@ def get_hesh(s, n, p, x):
 
 def robin_karp(s, search, p, x):
     ht = get_hesh(search, len(search), p, x)
-    hs = get_hesh(s, len(search), p, x)
+    hs = get_hesh(s, len(s), p, x)
     xt = 1
     for i in range(len(search)):
         xt = (xt*x) % p
-    for i in range(len(s)-len(search)+1):
+    for i in range(len(s)):
         if (ht == hs):
-            print(i, end=" ")
-        if (i + len(search) < len(s)):
-            hs = (hs*x-ord(s[i])*xt+ord(s[i+len(search)])) % p
+            return i
+        ht = (ht*x-ord(search[i])*xt+ord(search[i])) % p
+    return -1
 
 
 def main():
@@ -24,7 +24,7 @@ def main():
     search = input()
     p = 1e9 + 7
     x = 26
-    robin_karp(string, search, p, x)
+    print(robin_karp(string, search, p, x))
 
 
 main()
