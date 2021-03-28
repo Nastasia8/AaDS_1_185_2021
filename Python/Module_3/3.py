0eb0b8db90a3ee0c5656daca790cf4f52ec4d906
dunkanhigh <oleg.sainov.2002@bk.ru>
2021-03-28 20:12:37 +0300

    fail on 20 test

--- Python/Module_3/3.py
+++ Python/Module_3/3.py
@@ -8,45 +8,61 @@
 
 
 def prefix(s):
-    p = [0]*len(s)
-    for i in range(len(s)-1):
-        j = p[i]
-        while j > 0 and s[j] != s[i+1]:
-            j = p[j-1]
-        if s[j] == s[i+1]:
-            p[i+1] = j+1
-        else:
-            p[i+1] = j
-    return p
+    v = [0]*len(s)
+    for i in range(1, len(s)):
+        k = v[i-1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k-1]
+        if s[k] == s[i]:
+            k = k + 1
+        v[i] = k
+    return v
 
 
 def main():
     s = str(input())
     p = prefix(s)
+    n = len(p)
+    flag = True
+    k_zero = p.count(0)
     print(p)
-    if len(p) == 0:
-        print('')
-    elif len(p) == 1:
-        print(1)
-
-    elif p[-1] == 0:
-        print(1)
+    if n*[0] == p:
+        k = n
     else:
-        k = 0
-        while p[k] == 0:
-            k += 1
-        if k == 1 and p.count(0) > 1:
-            if p[k+1] == 0:
-                print(len(s)//(k+2))
+        k = 1
+        if n > 2:
+            if p[-1]-1 == p[-2]:
+                while p[k] == 0:
+                    k += 1
+                    flag = False
+                if k > p[-1]:
+                    k = n
+                elif max(p) != p[-1]:
+                    k = n
+                elif p[k] == p[k+1]:
+                    k += 1
+                elif k == k_zero and p[-1] % k_zero != 0:
+                    k = n
+                elif k_zero != k:
+                    if flag == False:
+                        k = n
+                    else:
+                        k = 1
+                        while p[k] != 0:
+                            k += 1
+                        while p[k] == 0:
+                            k += 1
+                        for i in range(k, n-1):
+                            if p[i] == p[i+1]:
+                                k += 1
+
+                        if n % k != 0:
+                            k = n
+                        elif p[-1] % k != 0:
+                            k = 1
             else:
-                print(1)
-        # elif k <=len(p):
-        elif 2*k <= len(p):
-            if p[k+1] == 1:
-                k += 1
-            print(len(s)//k)
-        else:
-            print(len(s)//k)
+                k = n
+    print(n//k)
 
 
 main()
