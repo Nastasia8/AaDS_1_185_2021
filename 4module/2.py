f2065ffd35d096439abc791e1e66c712f78d190a
zipliZ <xle64ik@gmail.com>
2021-04-01 18:14:10 +0300

    Create 2.py

---
+++ 4module/2.py
@@ -0,0 +1,22 @@
+n=int(input())
+s=list(map(int, input (). split (maxsplit=n)))
+stack=[]
+isPsk= True
+num=1
+for i in s:
+    for _ in range (len(stack)):
+        if stack and num == stack[-1]:
+            stack.pop()
+            num+=1
+    if i!=num:
+        if stack and i>stack[-1]:
+            isPsk= False
+            break
+        stack.append(i)
+    else:
+        num+=1
+if isPsk==True:
+    print ("YES")
+else:
+    print ("NO")
+
