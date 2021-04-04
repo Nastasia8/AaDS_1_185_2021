396daf3f6fb93c9f7632228161e02f1882367419
unknown <oleg.savinov.2002@bk.ru>
2021-04-04 19:56:06 +0300

    add 4.py

--- Python/Module_3/4.py
+++ Python/Module_3/4.py
@@ -1,5 +1,6 @@
 def prefix(s):
     v = [0]*len(s)
+
     for i in range(1, len(s)):
         k = v[i-1]
         while k > 0 and s[k] != s[i]:
@@ -13,28 +14,8 @@ def prefix(s):
 def main():
     s = input()
     p = prefix(s)
-    print(p)
     n = len(p)
-    res = 0
-    if 1 not in p:
-        k = s
-    else:
-        k = s[0:len(s) - p[::-1].index(1) - 1]
-    if s == k*(n//len(k)):
-        res = n//len(k)
-    else:
-        res = 1
-
-    print(res)
-    if res > 1:
-        print(n)
-    elif n % (n - p[-1]) != 0 and p[-2] <= p[-1]:
-        x = n - p[-1]
-        while n % x != 0:
-            n += 1
-        print(n)
-    else:
-        print(n)
+    print(n - p[-1])
 
 
 main()
