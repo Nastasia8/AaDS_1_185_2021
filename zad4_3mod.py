5bdf5cdb5a18b1c6689bd575b3c8803d5a88245d
anna165 <71322376+anna165@users.noreply.github.com>
2021-04-04 19:50:50 +0300

    Add files via upload

---
+++ zad4_3mod.py
@@ -0,0 +1,17 @@
+def prefix(s,l):
+    pref = [0] * l
+    for i in range(l - 1):
+        j = pref[i]
+        while (j>0) and (s[i+1] != s[j]):
+            j = pref[j-1]
+        if (s[i+1] == s[j]):
+            pref[i+1] = j + 1
+        else:
+            pref[i+1] = 0
+    return pref
+def main():
+    s = input()
+    l=len(s)
+    pref = prefix(s,l)
+    print(l - pref[-1])
+main()
