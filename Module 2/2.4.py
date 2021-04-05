a802de77f14662784b6c2706381acc071731da32
hvlll <hvlll@users.noreply.github.com>
2021-04-05 23:45:32 +0300

    YContest

--- Module 2/2.4.py
+++ Module 2/2.4.py
@@ -1,3 +1,5 @@
+count = 0
+
 def mergeSort(numbers, start, end):
 	if end - start > 1:
 		middle = (start + end) // 2
@@ -5,32 +7,36 @@ def mergeSort(numbers, start, end):
 		mergeSort(numbers, middle, end)
 		left = numbers[start: middle]
 		right = numbers[middle: end]
-		internalSort(numbers, left, right, start)
-		print(start + 1, end, numbers[start], numbers[end - 1])
+		internalSort(numbers, left, right, start) 
+
 
 def internalSort(arr, left, right, start):
 	i = j = 0
+	global count
 	k = start
 	while i < len(right) and j < len(left):
 		if left[j] > right[i]:
 			arr[k] = right[i]
-			i +=1
+			i += 1
+			count += len(left) - j
 		else:
 			arr[k] = left[j]
+			j += 1
 		k += 1
 	while j < len(left):
 		arr[k] = left[j]
 		j += 1
 		k += 1
 	while i < len(right):
-	    arr[k] = right(i)
+	    arr[k] = right[i]
 	    i += 1
 	    k += 1
 
+
 def main():
-	n = int(input())
-	numbers = list(map(int,input().split(maxsplit = n)))
+	m = int(input())
+	numbers = list(map(int, input().split(maxsplit = m)))
 	mergeSort(numbers, 0, len(numbers))
-	print(*numbers)
+	print(count)
 	
 main()
---
+++ Module 3/3.1.py
@@ -0,0 +1,25 @@
+def zFunct(s):
+    arr = [0] * len(s)
+    l = r = 0
+    for i in range(1, len(s)):
+        if i <= r:
+            arr[i] = min(arr[i - l], r - i + 1)
+        while arr[i] + i < len(s) and s[arr[i]] == s[arr[i] + i]:
+            arr[i] += 1
+        if arr[i] + i - 1 > r:
+            l = i
+            r = arr[i] + i - 1
+    return arr
+            
+
+def main():
+    s = input()
+    t = input()
+    z = zFunct(t + "#" + s)
+    for i in range(len(z)):
+        if len(t) == z[i]:
+            print(i - len(t) - 1, end = " ")
+
+
+main()
+    #ababbababa
