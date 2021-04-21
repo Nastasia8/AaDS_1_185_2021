95708b2a4bfbcaa5c16a6b6f9f4f63cf3fa001f9
DanielS005 <71259569+DanielS005@users.noreply.github.com>
2021-04-21 09:22:18 +0300

    Update module4_4.py

--- module4_4.py
+++ module4_4.py
@@ -1,14 +1,23 @@
-from collections import deque
-n = [int(i) for i in  input().split()]
-s = deque([int(i) for i in input().split()][:n[0]])
-m = 1e9 + 7
-mins = []
-while len(s) >= n[1]:
-    for i in range(n[1]):
-        if s[i] < m:
-            m = s[i]
-    mins.append(m)
-    m = 1e9 + 7
-    s.popleft()
+def find_min(n, k, el):
+    stk = []
+    for i in range(k):
+        while stk and el[1] <= arr[stk[-1]]:
+            stk.pop()
+        stack.append(1)
 
-[print(i) for i in mins]
+    for i in range(k, n):
+        print(el[stk[0]])
+        while stk and i - k >= stk[0]:
+            stk.pop(0)
+        while stk and arr[i] <= arr[stk[-1]]:
+            stk.pop()
+        stk.append(i)
+
+    print(el[stk[0]])
+
+
+
+
+n, k = map(int, input().split())
+el = [int(i) for i in input().split()][:n[0]]
+find_min(n, k, el)
