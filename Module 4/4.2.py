ce796a97626b258151705ebd8af64bb12baa04a2
hvlll <hvlll@users.noreply.github.com>
2021-04-28 14:52:41 +0300

    YContestUpd

--- Module 4/4.2.py
+++ Module 4/4.2.py
@@ -1,1 +1,21 @@
-#Work in progress
+n = int(input())
+string = list(map(int, input().split()))
+stack = []
+isPsk = True
+num = 1
+for i in string:
+    for j in range(len(stack)):
+        if stack and num == stack[-1]:
+            stack.pop()
+            num += 1
+    if i != num:
+        if stack and i > stack[-1]:
+            isPsk = False
+            break
+        stack.append(i)
+    else:
+        num += 1
+if isPsk:
+    print ("YES")
+else:
+    print ("NO")
--- Module 4/4.4.py
+++ Module 4/4.4.py
@@ -1,8 +1,19 @@
 n, k = map(int, input().split())
-lst = list(map(int, input().split()))
- 
+nums = list(map(int, input().split()))
+stack = []
+res = [0] * n
+for i in range(n):
+    while stack and nums[i] < nums[stack[-1]]:
+        res[stack.pop()] = i
+    stack.append(i)
+while stack:
+    res[stack.pop()] = n
+i_n = 0
 for i in range(n - k + 1):
-    print(min(lst[i:i + k]))
+    if i_n < i:
+        i_n = i
+    while res[i_n] < i - k:
+        i_n = res[i_n]
+    print(nums[i_n])
 
-
-#Work in progress
+#Вердикт: WA
