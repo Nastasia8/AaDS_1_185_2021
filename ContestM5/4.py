2d1e5c84156dc1e1a4a5c04b9819fcc98f6137a1
CruzedV <mr.dobrikv@mail.ru>
2021-05-07 12:38:57 +0300

    5.4 Update

--- ContestM5/4.py
+++ ContestM5/4.py
@@ -1,5 +1,14 @@
 
-
+"""
+5
+0 0 2 0 1
+5
+1 5 2
+2 4 2
+3 5 3
+1 1 1
+3 4 1
+"""
 def build(v, l, r, do, a):
     if r-l == 1:
         do[v] = a[l]
@@ -8,7 +17,6 @@ def build(v, l, r, do, a):
     build(2*v+1, l, m, do, a)
     build(2*v+2, m, r, do, a)
     do[v] = do[2*v+1] + do[2*v+2]
-    print(*do)
 
 def get_k_zero(v, l, r, do, k):
     if k > do[v]:
@@ -22,14 +30,13 @@ def get_k_zero(v, l, r, do, k):
         return get_k_zero(2*v+2, m+1, r, do, k - do[2*v+1])
 
 n = int(input())
-a = input().split()
-a = [int(a[i])*0 if int(a[i]) != 0 else int(a[i]) + 1 for i in range(n)]
+a = [0 if int(i) != 0 else 1 for i in input().split()]
 do = [0]*4*n
-build(0, 0, n, do, a, n)
+build(0, 0, n, do, a)
 q = int(input())
 index = []
 while q != 0:
     l, r, k = map(int, input().split())
-    index.append(get_k_zero(0, 0, n, do, k))
+    index.append(get_k_zero(0, l, r, do, k))
     q -= 1
 print(*index)
