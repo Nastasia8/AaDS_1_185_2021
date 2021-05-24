1d33d879403ba735000c0a257f9d0240378d7afa
Vlad <48482781+CruzedV@users.noreply.github.com>
2021-05-24 11:17:01 +0300

    6.2 Done

--- ContestM6/6.2.py
+++ ContestM6/6.2.py
@@ -1,87 +1,76 @@
-class Heap:
-    __heap = []
-        
-    @property
-    def length(self):
-        return len(self.__heap)
-        
-    @property
-    def empty(self):
-        return self.length == 0
-        
-    @property
-    def min(self):
-        return self.__heap[0]
-        
-    def clear(self):
-        return self.__heap.clear()
-        
-    def shift_down(self, value):
-        while 2*value+1 < self.length:
-            left_indx = 2*value+1
-            right_indx = 2*value+2
-            _indx = left_indx
-            if right_indx < self.length and self.__heap[left_indx] > self.__heap[right_indx]:
-                _indx = right_indx
-            if self.__heap[_indx] >= self.__heap[value]:
-                break
-            self.__heap[value], self.__heap[_indx] = self.__heap[_indx], self.__heap[value]
-            value = _indx
-    
-    def shift_up(self, value):
-        while value > 0 and self.__heap[value] < self.__heap[(value - 1)//2]:
-            self.__heap[value],self.__heap[(value - 1)//2] = self.__heap[(value - 1)//2], self.__heap[value]
-            value = (value - 1)//2
-    
-    def extract(self):
-        self.__heap[0], self.__heap[self.length-1] = self.__heap[self.length-1], self.__heap[0]
-        self.__heap.pop()
-        self.shift_down(0)
-    
-    def add(self, value):
-        self.__heap.append(value)
-        self.shift_up(self.length-1)
-    
-    def print_heap(self):
-        print(*self.__heap)
 
-def build(arr):
-    heap = arr[:]
-    h = Heap(heap)
-    for i in range(len(heap)-1, -1, -1):
-        h.shift_down(i)
-    return h
+def shift_up(index, heap):
+    while heap[index] > heap[(index-1)//2] and (index-1)//2 >= 0:
+        heap[index], heap[(index-1)//2] = heap[(index-1)//2], heap[index]
+        index = (index-1)//2
+    return index+1
+
+def shift_down(index, heap):
+    while 2*index+1 < len(heap):
+        left_index = 2*index+1
+        right_index = 2*index+2
+        child_index = left_index
+        if right_index < len(heap) and heap[left_index] < heap[right_index]:
+            child_index = right_index
+        if heap[child_index] <= heap[index]:
+            break
+        heap[index], heap[child_index] = heap[child_index], heap[index]
+        index = child_index
+    return index+1
+
+def add(item, heap):
+    heap.append(item)
+    return shift_up(len(heap)-1, heap)
+
+def direct_extract(index, heap):
+    val = heap[len(heap)-1]
+    heap[index-1], heap[len(heap)-1] = heap[len(heap)-1], heap[index-1]
+    ind = heap.pop()
+    if val < ind:
+        shift_down(index-1, heap)
+    elif val > ind:
+        shift_up(index-1, heap)
+    return ind
+    
+def extract(heap):
+    heap[0], heap[len(heap)-1] = heap[len(heap)-1], heap[0]
+    ind = heap.pop()
+    if heap:
+        return shift_down(0, heap), ind
+    else:
+        return 0, ind
+
+def get_max(heap):
+    return heap[0]
 
 def main():
-    n = int(input())
-    res = []
-    heap = Heap()
-    for i in range(n):
-        command = list(map(str, input().split()))
-        if command[0] == "ADD":
-           heap.add(int(command[1])) 
-        elif command[0] == "CLEAR":
-            heap.clear()
-        else:
-            if heap.empty:
-                res.append("CANNOT")
+    heap = []
+    result = []
+    input_ = input().split()
+    n, m = int(input_[0]), int(input_[1])
+    for i in range(m):
+        data = input().split()
+        if int(data[0]) == 1:
+            if not heap:
+                result.append(-1)
             else:
-                res.append(heap.min)
-                heap.extract()
-            
-    print(*res, sep="\n")
-    
+                result.append(extract(heap))
+        elif int(data[0]) == 2:
+            if len(heap) == n:
+                result.append(-1)
+            else:
+                result.append(add(int(data[-1]), heap))
+        elif int(data[0]) == 3:
+            if len(heap) >= int(data[-1]) and int(data[-1]) > 0:
+                result.append(direct_extract(int(data[-1]), heap))
+            else:
+                result.append(-1)
+
+    for i in result:
+        if type(i) == tuple:
+            print(*i)
+        else:
+            print(i)
+    print(*heap)
+
 main()
-'''
-ADD 192168812
-ADD 125
-ADD 321
-EXTRACT
-EXTRACT
-CLEAR
-ADD 7
-ADD 555
-EXTRACT
-EXTRACT
-EXTRACT
-'''
