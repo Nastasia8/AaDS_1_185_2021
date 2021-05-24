18c76f2a6aa02fc91a9d1fda29103dad5870bb16
svvan45 <71321364+svvan45@users.noreply.github.com>
2021-05-25 02:27:25 +0300

    Конетест

---
+++ k_3.3.py
@@ -0,0 +1,21 @@
+def prefix(s,l):
+    pref = [0] * l
+    v = [0]*len(s)
+    for i in range(1,len(s)):
+        k = v[i-1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k-1]
+        if s[k] == s[i]:
+            k = k + 1
+        v[i] = k
+    return v
+def main():
+    s = input()
+    l=len(s)
+    pfx = prefix(s,l)
+    if (l%(l-pfx[-1])==0):
+        print(l//(l-pfx[-1]))
+    else:
+        print("1")
+main()
+
