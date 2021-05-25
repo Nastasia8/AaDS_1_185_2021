75fecf1a7c5381dc2d6bd079e11a278d6427d9ef
CruzedV <mr.dobrikv@mail.ru>
2021-05-25 10:41:32 +0300

    5.4 Done

--- ContestM5/4.py
+++ ContestM5/4.py
@@ -1,14 +1,5 @@
 
-"""
-5
-0 0 2 0 1
-5
-1 5 2
-2 4 2
-3 5 3
-1 1 1
-3 4 1
-"""
+
 def build(v, l, r, do, a):
     if r-l == 1:
         do[v] = a[l]
@@ -22,12 +13,22 @@ def get_k_zero(v, l, r, do, k):
     if k > do[v]:
         return -1
     if r - l == 1:
-        return l
+        return r
     m = (r+l)//2
     if do[2*v+1] >= k:
         return get_k_zero(2*v+1, l, m, do, k)
     else:
         return get_k_zero(2*v+2, m, r, do, k - do[2*v+1])
+
+def get_sum(v, l, r, do, ql, qr):
+    if ql <= l and qr >= r:
+        return do[v]
+    if ql >= r or qr <= l:
+        return 0
+    m = (r+l)//2
+    tl = get_sum(2*v+1, l, m, do, ql, qr)
+    tr = get_sum(2*v+2, m, r, do, ql, qr)
+    return tl + tr
 
 n = int(input())
 a = [0 if int(i) != 0 else 1 for i in input().split()]
@@ -37,6 +38,12 @@ q = int(input())
 index = []
 while q != 0:
     l, r, k = map(int, input().split())
-    index.append(get_k_zero(0, l-1, r, do, k))
+    sum_ = get_sum(0, 0, n, do, l-1, r)
+    if sum_ >= k and l > 1:
+        index.append(get_k_zero(0, 0, n, do, k+get_sum(0, 0, n, do, 0, l-1)))
+    elif sum_ >= k and l == 1:
+        index.append(get_k_zero(0, 0, n, do, k))
+    else:
+        index.append(-1)
     q -= 1
 print(*index)
