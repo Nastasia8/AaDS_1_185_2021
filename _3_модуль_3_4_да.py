b6af321e491059ba2416f4699aef23474d89e01c
NarutoUzumaki <71786909+Juuzoo@users.noreply.github.com>
2021-05-27 12:28:37 +0300

    Add files via upload

---
+++ _3_модуль_3_4_да.py
@@ -0,0 +1,38 @@
+#3
+def prefix(A, n):
+    res = [0]*n
+    res[0] = 0
+    for i in range(n-1):
+        j = res[i]
+        while (j > 0 and A[i+1] != A[j]):
+            j = res[j-1]
+        if (A[i+1] == A[j]):
+            res[i+1] = j + 1
+        else:
+            res[i+1] = 0
+    return res
+
+A = input()
+res = prefix(A, len(A))
+k = len(A) - res[len(A)-1]
+if (len(A) % k == 0):
+    print(len(A)//k)
+else:
+    print(1)
+#4
+def prefix_func(k, n):
+    s_res = [0]*n
+    s_res[0] = 0
+    for i in range(n-1):
+        j = s_res[i]
+        while (j > 0 and k[i+1] != k[j]):
+            j = s_res[j-1]
+        if (k[i+1] == k[j]):
+            s_res[i+1] = j + 1
+        else:
+            s_res[i+1] = 0
+    return s_res
+
+S = input()
+k = prefix_func(S, len(S))
+print(len(S) - k[len(k)-1])
