3c62e8df8b3d75680a6886afea81bb428a5eeec9
Nikita3221488 <71322132+Nikita3221488@users.noreply.github.com>
2021-05-28 17:31:33 +0300

    Update 3.4.py

--- 3.4.py
+++ 3.4.py
@@ -1,3 +1,4 @@
+'''
 n = int(input()) 
 num = list(map(int, input().split()))[:n] 
 stack = [] 
@@ -19,3 +20,21 @@ print(*indexes)
 9
 добавление елемента в стек
 10 вывод
+'''
+def prefix(s):
+    v = [0]*len(s)
+    count = 1
+    for i in range(1,len(s)):
+        k = v[i-1]
+        while k > 0 and s[k] != s[i]:
+            k = v[k-1]
+        if s[k] == s[i]:
+            k = k + 1
+        else:
+            k = 0
+        v[i] = k
+        if v[i] < 2:
+            count = i
+    return count
+s = input()
+print(prefix(s))
